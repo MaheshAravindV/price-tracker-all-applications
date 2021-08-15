00bf0c9897aaac7495fa00ae07376103477e906a
MaheshAravindV <mahesharavind@student.tce.edu>
2021-08-16 04:13:25 +0530

    Changed time of reminder

--- price-tracker-bot/index.js
+++ price-tracker-bot/index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const Cronjob = require("cron").CronJob;
-new Cronjob("0 0 0 * * *", () => {
+new Cronjob("0 0 9 * * *", () => {
   require("./dailyReminder");
 }).start();
 
